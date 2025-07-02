1e7908a6111abaf66dc22fe15448ec22919b0278
munaziri123 <munajosue@gmail.com>
2025-07-01 22:26:02 -0700

    resolving nav bar naming convetion

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Navbar.css';
+import './navbar.css';
 import { FaSearch, FaUserCircle, FaBell, FaBars, FaTimes } from 'react-icons/fa';
 import logo from '../../assets/react.jpg';
 
