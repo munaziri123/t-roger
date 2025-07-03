f446c3184eadf812e65c372df559d6e5e777bb46
munaziri123 <munajosue@gmail.com>
2025-07-03 07:55:13 -0700

    working on payment

--- src/components/model/payment_model.jsx
+++ src/components/model/payment_model.jsx
@@ -21,13 +21,7 @@ const PaymentModal = ({ onClose }) => {
         <p className="modal-description">
           Once your payment is received, your <strong>Performer Badge</strong> and <strong>Entry Letter</strong> will be sent to you via email. Thank you for your understanding and patience.
         </p>
-
-        <p className="code-label">MoMo Code yo kwishyuriraho:</p>
-        <div className="code-box">123456</div>
-
-        <p className="modal-description">Cyangwa andika numero yawe ya MTN hano:</p>
-        <input type="text" placeholder="ex: 078xxxxxxx" className="mtn-input" />
-        <button className="pay-now">Ishyura 10,000 RWF</button>
+        <button className="pay-now" disabled>Ishyura 10,000 RWF</button>
       </div>
     </div>
   );
