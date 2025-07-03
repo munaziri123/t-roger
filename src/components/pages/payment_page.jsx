0509965904187cf1d48ee822509932b1d0ffe0fb
munaziri123 <munajosue@gmail.com>
2025-07-03 07:23:15 -0700

    working on payment

--- src/components/pages/payment_page.jsx
+++ src/components/pages/payment_page.jsx
@@ -16,9 +16,21 @@ const PaymentPage = () => {
   return (
     <div className="payment-container">
       <div className="confirmation-box">
-        <h2>Dosiye yawe isaba icyemezo cy’amavuko yakozwe neza</h2>
+        <h2>Registration Payment Required</h2>
+        
+        <p>
+          You have been successfully registered to perform at the upcoming event, proudly sponsored by the <strong>T-Roger Family</strong>.
+        </p>
+        <p>
+          To receive your <strong>Performer Badge</strong> and <strong>Welcome Letter</strong>, you are required to complete your registration payment.
+        </p>
+        <p>
+          Please click <strong>"Ishyura"</strong> below to proceed with payment.
+        </p>
+
         <p><strong>Kode yo kwishyuriraho:</strong> 880703239619</p>
         <p><strong>Ikiguzi cya serivisi:</strong> RWF 500</p>
+        
         <button onClick={handleOpenModal} className="pay-btn">Ishyura</button>
       </div>
 
